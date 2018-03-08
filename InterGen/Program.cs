d5c5827e0279f1443060e7ce05949caf4569001d
Renato Suga <rennyatwork@gmail.com>
2018-03-07 20:24:27 -0500

    Add project files.

---
+++ InterGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
